73aaeaf15acfabf365814af6cd1fb4a4598ac93c
Abhishek Kumar <sahniak56@gmail.com>
2023-01-02 01:12:39 +0530

    g

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-// import ErrorMiddleware from "./middlewares/Error.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -26,16 +25,6 @@ app.use(
   })
 );
 
-// Importing & Using Routes
-// import course from "./routes/courseRoutes.js";
-// import user from "./routes/userRoutes.js";
-// import payment from "./routes/paymentRoutes.js";
-// import other from "./routes/otherRoutes.js";
-
-// app.use("/api/v1", course);
-// app.use("/api/v1", user);
-// app.use("/api/v1", payment);
-// app.use("/api/v1", other);
 
 export default app;
 
